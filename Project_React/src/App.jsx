a384f6788c0b0c060d8cf7262f44ab1e69dae1d6
adikarais <103338766+adikarais@users.noreply.github.com>
2024-11-18 11:59:06 +0700

    Add files via upload

--- Project_React/src/App.jsx
+++ Project_React/src/App.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import Privacy_Policy from './pages/Privacy_Policy';
 import About_us from './pages/About_us';
 // import Profile_Page_komunitas from './pages/Profile_Page_komunitas';
-import pusat_bantuan from './pages/pusat_bantuan';
+import Term_of_Condition_comunity from './pages/Term_of_Condition_comunity';
+import Term_of_Condition from './pages/Term_of_Condition';
 // tambah page lain
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -13,7 +14,8 @@ function App() {
         <Route path="/privacy_policy" element={<Privacy_Policy />} />
         <Route path="/About_us" element={<About_us />} />
         {/* <Route path="/Profile_Page_komunitas" element={<Profile_Page_komunitas />} /> */}
-        <Route path="/pusat_bantuan" element={<pusat_bantuan />} />
+        <Route path="/Term_of_Condition_comunity" element={<Term_of_Condition_comunity />} />
+        <Route path="/Term_of_Condition" element={<Term_of_Condition />} />
       </Routes>
     </BrowserRouter>
   );
