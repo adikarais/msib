5aede05de865118289209dcd9dcea2dec8e22f47
nabilladdian <nabillaazizah19@gmail.com>
2024-12-20 01:06:58 +0700

    first commit

--- Project_React/vite.config.js
+++ Project_React/vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    port: 5174, // Ganti dengan port yang diinginkan
-  },
   plugins: [react()],
-});
+})
